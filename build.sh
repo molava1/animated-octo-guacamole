387ddc1207a1ab0c90614439fac6da89beed28f6
molava1 <103011185+molava1@users.noreply.github.com>
2022-04-26 19:17:45 -0700

    Add files via upload

---
+++ build.sh
@@ -0,0 +1,6 @@
+cat templates/top.html templates/bottom.html content/index.html >docs/index.html
+
+cat templates/top.html templates/bottom.html content/about_me.html >docs/about_me.html
+
+cat templates/top.html templates/bottom.html content/contact_me.html >docs/contact_me.html
+
